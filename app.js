3f4cd7dcb4e6201bb0d3fde2424676b291629db4
sierra236 <dogaan.ds@gmail.com>
2022-09-29 17:27:48 +0300

    connected db & post posts

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const ejs= require('ejs');
+const path = require('path');
+const Post = require("./models/Post");
 
 const app = express();
 
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
 //MIDDLEWARES
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-
-  res.send(blog);
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts
+  });
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.post('/blog', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/')
+  });
+  
+  
 
 const port = 2500;
 app.listen(port, () => {
